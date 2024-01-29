1a28c0d747fcef112200f5001f24886eaa5f9043
putilovskaya <putilovskayaa@mail.ru>
2024-01-29 23:54:18 +0300

    fix channel.py

--- src/channel.py
+++ src/channel.py
@@ -17,9 +17,58 @@ class Channel:
         self.title = response['items'][0]['snippet']['title']
         self.description = response['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + response['items'][0]['id']
-        self.subscriber_count = response['items'][0]['statistics']['subscriberCount']
-        self.video_count = response['items'][0]['statistics']['videoCount']
-        self.view_count = response['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(response['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(response['items'][0]['statistics']['videoCount'])
+        self.view_count = int(response['items'][0]['statistics']['viewCount'])
+
+    def __str__(self):
+        """
+        Отображает информацию о канале для пользователей
+        :return: <название_канала> (<ссылка_на_канал>)
+        """
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        """
+        Складывает количество подписчиков канала1 и канала2
+        """
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """
+        Вычитает из количества подписчиков канала1 количество подписчиков канала2
+        """
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        """
+        Проверяет, является ли количество подписиков канала1 меньше подписчиков канала2
+        """
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """
+        Проверяет, является ли количество подписиков канала1 меньше или равно подписчиков канала2
+        """
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        """
+        Проверяет, является ли количество подписиков канала1 больше подписчиков канала2
+        """
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """
+        Проверяет, является ли количество подписиков канала1 больше или равно подписчиков канала2
+        """
+        return self.subscriber_count >= other.subscriber_count
+
+    def __eq__(self, other):
+        """
+        Проверяет, является ли количество подписчиков канала1 и канала2 одинаковым
+        """
+        return self.subscriber_count == other.subscriber_count
 
     @property
     def channel_id(self):
