c99844b75349319ba480cde24bca63e1c459b24b
putilovskaya <putilovskayaa@mail.ru>
2024-02-10 14:26:55 +0300

    add class MixinYouTube to channel.py

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,21 @@ import os
 from googleapiclient.discovery import build
 
 
-class Channel:
+class MixinYouTube:
+    """Вспомогательный класс для работы с YouTube API"""
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        api_key: str = os.getenv('YT_API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+
+class Channel(MixinYouTube):
     """Класс для YouTube-канала"""
 
     def __init__(self, channel_id: str) -> None:
@@ -86,21 +100,9 @@ class Channel:
         response = self.get_info_about_channel()
         print(json.dumps(response, indent=2, ensure_ascii=False))
 
-    @classmethod
-    def get_service(cls):
-        """
-        Возвращает объект для работы с YouTube API
-        """
-        api_key: str = os.getenv('YT_API_KEY')
-        return build('youtube', 'v3', developerKey=api_key)
-
     def to_json(self, filename):
         """
         Сохраняет в файл значения атрибутов экземпляра `Channel`
         """
         with open(filename, 'w', encoding='UTF-8') as file:
             json.dump(self.__dict__, file, ensure_ascii=False, indent=2)
-
-
-
-
